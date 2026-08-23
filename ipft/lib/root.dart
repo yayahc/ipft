413fffd59c323f462005e848b43bbd1151fdc71d
Yaya HC <hamedcuenca5@gmail.com>
2026-08-23 06:12:06 +0000

    chore: start main feature

---
+++ ipft/lib/root.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Root extends StatelessWidget {
+  const Root({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
+}
